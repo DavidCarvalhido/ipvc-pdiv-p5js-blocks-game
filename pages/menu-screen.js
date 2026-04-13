2765700ad675a90649eae8f97d596729d961d9c7
DavidCarvalhido <davidcarvalhido@hotmail.com>
2026-04-13 23:49:09 +0100

    build: add navigation and game state management

---
+++ pages/menu-screen.js
@@ -0,0 +1,10 @@
+function menuScreenDraw() {
+    fill(255)
+    textAlign(CENTER, CENTER)
+
+    textSize(50)
+    text("MEU JOGO", width / 2, height / 3)
+
+    textSize(20)
+    text("Pressiona ENTER para começar", width / 2, height / 2)
+}
--- sketch.js
+++ sketch.js
@@ -1,9 +1,41 @@
 loadScript("./p5.min.js")
+loadScript("./pages/menu-screen.js")
+loadScript("./pages/game-screen.js")
+loadScript("./pages/gameover-screen.js")
+
+// Variáveis globais
+let gameState = "menu"
+let score = 0
 
 function setup() {
-  createCanvas(1200, 600);
+    createCanvas(1200, 600)
+    noCursor()
+    rectMode(CENTER)
+    textSize(30)
+    noStroke()
+    fill(33)
 }
 
 function draw() {
-  background(220);
+    background(220)
+
+    if (gameState === "menu") {
+        menuScreenDraw()
+    } else if (gameState === "game") {
+        playGameScreenDraw()
+    } else if (gameState === "gameover") {
+        gameOverScreenDraw()
+    }
+}
+
+function keyPressed() {
+    if (gameState === "menu" && (keyCode === ENTER || keyCode === 32)) {
+        gameState = "game"
+        score = 0
+    }
+
+    if (gameState === "gameover" && (keyCode === ENTER || keyCode === 32)) {
+        gameState = "menu"
+        score = 0
+    }
 }
