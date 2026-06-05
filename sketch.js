4bc91149179ad0f4ed1e77ca5919d3d2cc8732c5
DavidCarvalhido <davidcarvalhido@hotmail.com>
2026-06-05 22:57:49 +0100

    build: sounds and backgroud music on the game

--- sketch.js
+++ sketch.js
@@ -11,6 +11,13 @@ let paddleHits = 0
 let level = 1
 let nextLevelReady = false
 
+let menuMusic
+let gameMusic
+let levelTransitionMusic
+let lifeLostSound
+let blockHitSound
+let gameOverSound
+
 function preload() {
     gameFont = loadFont("./assets/fonts/PressStart2P-Regular.ttf")
     menuImage = loadImage("./assets/images/menu-background.jpg")
@@ -19,6 +26,14 @@ function preload() {
 
     // para o jogo
     levelOneBackground = loadImage("./assets/images/level1-background.png")
+
+    //audio
+    menuMusic = loadSound("./assets/audio/freesound_community-retro-synth-loop-128-41048.mp3")
+    gameMusic = loadSound("./assets/audio/freesound_community-retro-wave-style-track-59892.mp3")
+    levelTransitionMusic = loadSound("./assets/audio/freesound_community-game-over-38511.mp3")
+    lifeLostSound = loadSound("./assets/audio/53439420-retro-hurt-sound-01-474779.mp3")
+    gameOverSound = loadSound("./assets/audio/kuzu420-game-over-284367.mp3")
+    blockHitSound = loadSound("./assets/audio/driken5482-retro-explode-1-236678.mp3")
 }
 
 function setup() {
@@ -40,13 +55,37 @@ function draw() {
     background(28, 28, 29)
 
     if (gameState === "menu") {
+        if (!menuMusic.isPlaying()) {
+            menuMusic.setVolume(0.25)
+            menuMusic.loop()
+        }
+        if (gameOverSound && gameOverSound.isPlaying()) {
+            gameOverSound.stop()
+        }
         menuScreen.draw()
     } else if (gameState === "game") {
+        if (menuMusic.isPlaying()) {
+            menuMusic.stop()
+        }
         gameScreen.draw()
     } else if (gameState === "leveltransition") {
+        if (!levelTransitionMusic.isPlaying()) {
+            if (gameMusic.isPlaying()) {
+                gameMusic.stop()
+            }
+            levelTransitionMusic.setVolume(0.35)
+            levelTransitionMusic.play()
+        }
         levelTransitionScreen.draw()
     } else if (gameState === "gameover") {
+        if (gameMusic.isPlaying()) {
+            gameMusic.stop()
+        }
         gameOverScreen.draw()
+        if (gameOverSound && !gameOverSound.isPlaying()) {
+            gameOverSound.setVolume(0.9)
+            gameOverSound.play()
+        }
     }
 }
 
@@ -63,11 +102,25 @@ function keyPressed() {
         paddleHits = 0
         level = 1
         gameScreen = new GameScreen()
+
+        if (!gameMusic.isPlaying()) {
+            gameMusic.setVolume(0.3)
+            gameMusic.loop()
+        }
     }
 
     if (gameState === "leveltransition" && (keyCode === ENTER || keyCode === 32)) {
         gameScreen = new GameScreen()
         gameState = "game"
+
+        if (levelTransitionMusic.isPlaying()) {
+            levelTransitionMusic.stop()
+        }
+
+        if (!gameMusic.isPlaying()) {
+            gameMusic.setVolume(0.3)
+            gameMusic.loop()
+        }
     }
 
     if (gameState === "gameover" && (keyCode === ENTER || keyCode === 32)) {
